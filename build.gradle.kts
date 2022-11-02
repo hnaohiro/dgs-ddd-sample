b53c58dd6fb62dfaf73988bf38d88a2cde1b81d8
hnaohiro <hasegawanaohiro1989@gmail.com>
2022-11-03 02:06:23 +0900

    introduce kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ subprojects {
     dependencies {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.3")
     }
 
     java.sourceCompatibility = JavaVersion.VERSION_17
