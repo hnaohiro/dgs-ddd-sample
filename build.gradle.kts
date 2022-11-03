cf3145a3a6e5a9a80863614e683a668ffe0745e1
hnaohiro <hasegawanaohiro1989@gmail.com>
2022-11-04 01:25:22 +0900

    introduce mockk

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         testImplementation("io.kotest:kotest-runner-junit5-jvm:5.5.3")
+        testImplementation("io.mockk:mockk:1.13.2")
     }
 
     java.sourceCompatibility = JavaVersion.VERSION_17
