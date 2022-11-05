f9b15c22630f1d6d5bf297420f4c6833d57cc701
hnaohiro <hasegawanaohiro1989@gmail.com>
2022-11-05 15:54:31 +0900

    add CreateUsecaseTest

---
+++ usecase/src/test/kotlin/com/dgsdddsample/usecase/show/CreateShowUsecaseTest.kt
@@ -0,0 +1,65 @@
+package com.dgsdddsample.usecase.show
+
+import com.dgsdddsample.domain.show.*
+import com.dgsdddsample.usecase.transaction.NopTransactionManager
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+
+class CreateShowUsecaseTest : BehaviorSpec() {
+    private val showRepository = mockk<ShowRepository>()
+    private val showFactory = mockk<ShowFactory>()
+    private val createShowUseCase = CreateShowUseCase(NopTransactionManager(), showRepository, showFactory)
+
+    init {
+        Given("handle") {
+            When("normal scenario") {
+                val show = Show(
+                    id = ShowId.from("01GGXJJSBWQZYP8FZAGEB4YWEW"),
+                    version = ShowVersion.init,
+                    title = Title("test"),
+                    releaseYear = ReleaseYear(2022),
+                )
+                every { showFactory.build(Title("test"), ReleaseYear(2022)) } returns show
+                every { showRepository.save(any()) } returns true
+
+                Then("returns valid show") {
+                    CreateShowUseCase
+                        .Params("test", 2022)
+                        .let { createShowUseCase.handle(it) }
+                        .shouldBe(CreateShowUseCase.DTO(show))
+                }
+            }
+
+            When("showFactory.build throws IllegalArgumentException") {
+                every { showFactory.build(any(), any()) } throws IllegalArgumentException()
+
+                Then("throws IllegalArgumentException") {
+                    shouldThrow<IllegalArgumentException> {
+                        createShowUseCase.handle(CreateShowUseCase.Params("test", 2022))
+                    }
+                }
+            }
+
+            When("showRepository.save returns false") {
+                val show = Show(
+                    id = ShowId.from("01GGXJJSBWQZYP8FZAGEB4YWEW"),
+                    version = ShowVersion.init,
+                    title = Title("test"),
+                    releaseYear = ReleaseYear(2022),
+                )
+                every { showFactory.build(Title("test"), ReleaseYear(2022)) } returns show
+                every { showRepository.save(any()) } returns false
+
+                Then("returns DTO(show is null)") {
+                    CreateShowUseCase
+                        .Params("test", 2022)
+                        .let { createShowUseCase.handle(it) }
+                        .shouldBe(CreateShowUseCase.DTO(error = "failed to save"))
+                }
+            }
+        }
+    }
+}
